4e6bf9fab58879613fac3bd2976439580abc7e7e
Ross Carlton <ross.evan.carlton@gmail.com>
2019-07-24 02:55:02 -0500

    button highlight pattern

--- mainjs.js
+++ mainjs.js
@@ -1,1 +1,25 @@
+window.onload = function (){
 
+
+    function highlight(){
+     
+        let setbtn = document.getElementById("setbutton");
+         setbtn.setAttribute("fill","#868686");
+         
+     }
+
+     function unlightsetbtn(){
+     
+        let setbtn = document.getElementById("setbutton");
+         setbtn.setAttribute("fill","#606060");
+         
+     }
+
+
+
+    var setbtnlight = document.getElementById("setbutton"); 
+
+     setbtnlight.addEventListener("mousedown",highlight);
+     document.addEventListener("mouseup",unlightsetbtn);
+
+}

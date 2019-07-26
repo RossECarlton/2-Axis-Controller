8a1aef3fbb06bf6ef1d215555418537d8e7d9acb
Ross Carlton <ross.evan.carlton@gmail.com>
2019-07-25 22:05:15 -0500

    further button highlights

--- mainjs.js
+++ mainjs.js
@@ -17,9 +17,56 @@ window.onload = function (){
 
 
 
+     
+    function rhighlight(){
+     
+        let rbtn = document.getElementById("releaseb");
+         rbtn.setAttribute("fill","#868686");
+         
+     }
+     
+     function runlight(){
+     
+        let rbtn = document.getElementById("releaseb");
+         rbtn.setAttribute("fill","#606060");
+         
+     }
+
+
+
+
+
+
+
+
+     function olight(){
+     
+        let rbtn = document.getElementById("open");
+         rbtn.setAttribute("fill","#868686");
+         
+     }
+     
+     function ounlight(){
+     
+        let obtn = document.getElementById("open");
+         obtn.setAttribute("fill","#606060");
+         
+     }
+
+
+
+
+    var obtnlight = document.getElementById("open");
+    var rbtnlight = document.getElementById("releaseb");  
     var setbtnlight = document.getElementById("setbutton"); 
 
      setbtnlight.addEventListener("mousedown",highlight);
      document.addEventListener("mouseup",unlightsetbtn);
+
+     rbtnlight.addEventListener("mousedown",rhighlight);
+     rbtnlight.addEventListener("mouseup",runlight);
+
+     obtnlight.addEventListener("mousedown",olight);
+     obtnlight.addEventListener("mouseup",ounlight);
 
 }

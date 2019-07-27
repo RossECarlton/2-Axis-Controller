3f45003cab5e9c6bb4540afc53db5810b7550206
Ross Carlton <ross.evan.carlton@gmail.com>
2019-07-26 19:51:57 -0500

    add drag functionality to lr axis

--- mainjs.js
+++ mainjs.js
@@ -97,7 +97,7 @@ window.onload = function (){
 
 
 
-     var lrbtnlight = document.getElementById("left-r-btn");
+     var lrbtn = document.getElementById("left-r-btn");
      var pwrbtnlight = document.getElementById("powersldr");
 
 
@@ -115,10 +115,43 @@ window.onload = function (){
      obtnlight.addEventListener("mousedown",olight);
      document.addEventListener("mouseup",ounlight);
 
-     lrbtnlight.addEventListener("mousedown",lrlight);
+     lrbtn.addEventListener("mousedown",lrlight);
      document.addEventListener("mouseup",lrunlight);
 
      pwrbtnlight.addEventListener("mousedown",plight);
      document.addEventListener("mouseup",punlight);
 
+
+
+
+
+
+     
+     // animate left right turn button 
+
+     function leftrightdrag (event){
+        let svgbtnrl4 = document.getElementById("left-r-btn");
+    
+        if (svgbtnrl4.getAttribute("fill")== "#F6CCA0"){
+            var WinW = window.innerWidth;
+            scaleFactor = 1200/WinW;
+            var y = event.clientY;
+            var M = y * scaleFactor;
+            
+            if (M < 270 ){
+                M = 270;
+            }
+            if (M > 950 ){
+                M = 950;
+            }
+
+            lrbtn.setAttribute("d", "M"+(M- 35)+",1567c0,8.25-6.75,15-15,15h-51c-8.25,0-15-6.75-15-15v-50c0-8.25,6.75-15,15-15h51c8.25,0,15,6.75,15,15V1567z");
+            }
+
+           
+    
+    }
+
+    lrbtn.addEventListener("mousemove", leftrightdrag);
 }
+
